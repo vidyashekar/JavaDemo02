98d52814d1c67cb4419587699f686fe56be34f73
HP <HP@VidyaShekar>
2023-02-05 22:50:03 +0800

    program on access control

---
+++ Dec2022/src/AccessControlDemo.java
@@ -0,0 +1,17 @@
+
+public class AccessControlDemo {
+    
+	int  a; // default access 
+	public int b; // public access 
+	private int c ; //private access
+	void setC(int i)
+	{
+		c=i;
+	}
+	int getc()
+	{
+		return c;
+	}
+	
+
+}
